119743c145909f1aecd7e10922d0bfd8bf84052e
João Vitor <joaovitorfelixcontato@gmail.com>
2024-11-26 16:22:42 -0300

    feat(backend): add cors middleware

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,11 @@
+import cors from "cors";
 import express from "express";
 
 import { rideRouter } from "./routes/ride";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/ride", rideRouter);
 
 app.listen(process.env.PORT, () => {
