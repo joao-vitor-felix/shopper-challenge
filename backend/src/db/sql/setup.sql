addc6a8eabd82cd04971eeb70b5698178b57e7fa
João Vitor <joaovitorfelixcontato@gmail.com>
2024-11-25 22:08:25 -0300

    chore: update setup sql

--- backend/src/db/sql/setup.sql
+++ backend/src/db/sql/setup.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE ride_app;
-
 CREATE TABLE
   IF NOT EXISTS customer (
     id VARCHAR(50) NOT NULL DEFAULT gen_random_uuid (),
@@ -30,7 +28,7 @@ CREATE TABLE
     comment TEXT NOT NULL,
     created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
-    FOREIGN KEY (driver_id) REFERENCES driver (id),
+    FOREIGN KEY (driver_id) REFERENCES driver (id) ON DELETE CASCADE,
     FOREIGN KEY (customer_id) REFERENCES customer (id)
   );
 
@@ -53,3 +51,59 @@ CREATE TABLE
 CREATE INDEX IF NOT EXISTS idx_customer_id ON ride (customer_id);
 
 CREATE INDEX IF NOT EXISTS idx_driver_id ON ride (driver_id);
+
+INSERT INTO
+  customer (id, name)
+VALUES
+  ('41b37ad0-9225-4b8e-8ed4-dba666c67cdc', 'Alice') ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO
+  driver (
+    name,
+    description,
+    vehicle,
+    tax_rate,
+    min_acceptable_meters
+  )
+VALUES
+  (
+    'Homer Simpson',
+    'Olá! Sou o Homer, seu motorista camarada! Relaxe e aproveite o passeio, com direito a rosquinhas e boas risadas (e talvez alguns desvios).',
+    'Plymouth Valiant 1973 rosa e enferrujado',
+    2.50,
+    1000
+  );
+
+INSERT INTO
+  driver (
+    name,
+    description,
+    vehicle,
+    tax_rate,
+    min_acceptable_meters
+  )
+VALUES
+  (
+    'Dominic Toretto',
+    'Ei, aqui é o Dom. Pode entrar, vou te levar com segurança e rapidez ao seu destino. Só não mexa no rádio, a playlist é sagrada',
+    'Dodge Charger R/T 1970 modificado',
+    5.00,
+    5000
+  );
+
+INSERT INTO
+  driver (
+    name,
+    description,
+    vehicle,
+    tax_rate,
+    min_acceptable_meters
+  )
+VALUES
+  (
+    'James Bond',
+    'Boa noite, sou James Bond. À seu dispor para um passeio suave e discreto. Aperte o cinto e aproveite a viagem.',
+    'Aston Martin DB5 clássico',
+    10.00,
+    10000
+  );
